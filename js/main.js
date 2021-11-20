56c76d6f9e701cfd6f56680c6bfaa18aa3432c7e
Михаил Луканин <mlukanin@ics.perm.ru>
2021-11-20 20:22:42 +0500

    Signed-off-by: Михаил Луканин <mlukanin@ics.perm.ru>

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
