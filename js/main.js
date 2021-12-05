0bd21355ae0b68661a2bb97261780ca07ac79bfe
Михаил Луканин <mlukanin@ics.perm.ru>
2021-12-05 23:22:52 +0500

    Добавил поддержку WebApi

--- js/main.js
+++ js/main.js
@@ -8,4 +8,28 @@ $(document).ready(function(){
             clickable: true,
         },
     });
+    $.ajax({
+        url: "https://localhost:44354/ShaurmaList"
+      }).done(function(data) {
+        let cardConstructor = "";
+        data.Data.forEach(element => {
+            cardConstructor = cardConstructor 
+            + "<div class='col s12 m4'>"
+                +"<div class='card'>"
+                    +"<div class='card-image'>"
+                        +"<img src='img/shaurma1.jpg'>"
+                        +"<span class='card-title'>"+element.Name+"</span>"
+                    +"</div>"
+                    +"<div class='card-content'>"
+                        +"<p>"+element.Description+"</p>"
+                    +"</div>"
+                    +"<div class='card-action'>"
+                        +"<a href='#'>Купить</a>"
+                        +"<div class='price'>"+element.Price+"</div>"
+                    +"</div>"
+                +"</div>"
+            +"</div>";
+        });
+        $('.menu-container').html(cardConstructor);
+      });
 });
